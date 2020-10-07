e54335f53232495f9ccfb060180feeca0758be69
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-10-07 20:15:56 +0100

    portfolio update colour

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -5,12 +5,12 @@
       <h2 class="title">PORTFOLIO</h2>
     </div>
 
-    <div class="projects-container">  
-      <div class="projects-block-container">     
+    <div class="project-container">  
+      <div class="project-block-container">     
         <img class="media-object" src="../assets/solar-system.png" alt="solar system project image"/>
       </div>
     
-      <div class="project-block-container">
+      <div class="project-block-container solar">
         <h4 class="project-heading">Solar System App</h4>
           <p>
             An interactive online browser application for the BBC targeted towards children. This app displays information on the planets in our solar system in a fun and interactive manner, with the information obtained from an external API.
@@ -18,7 +18,7 @@
           </p>
       </div>
 
-      <div class="project-block-container">
+      <div class="project-block-container gym">
         <h4>Total Fitness Gym App</h4>
           <p>
             A web application for a gym manager to manage memberships, classes and sessions. Built using Ruby and Sinatra, and writing data to an SQL database.
@@ -26,15 +26,15 @@
           </p>
       </div>
 
-      <div class="projects-block-container">    
+      <div class="project-block-container">    
           <img class="media-object" src="../assets/gym-app.png" alt="gym app project image"/>
       </div>
 
-      <div class="projects-block-container">     
+      <div class="project-block-container">     
         <img class="media-object" src="../assets/tyrannogotchi.png" alt="tyrannogotchi project image"/>
       </div>
     
-      <div class="project-block-container">
+      <div class="project-block-container tyrannogotchi">
         <h4 class="project-heading">Tyrannogotchi</h4>
           <p>
             A Tamagotchi-inspired web application where a user can create an account and take care of a virtual T-Rex by feeding and healing them.
@@ -77,32 +77,33 @@ export default {
   padding: 0.5rem;
 }
 
-.projects-container {
+.project-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
 
-.projects-block-container {
-  margin: 1.5rem;
+.project-block-container {
+  /* margin: 1rem; */
+  background-color: rgb(4, 48, 88);;
 }
 
 .media-object {
   width: 100%;
   height: auto;
   text-align: center;
-  box-shadow: 5px 5px 15px hsla(210, 6%, 42%, 0.73);
+  /* box-shadow: 5px 5px 15px hsla(210, 6%, 42%, 0.73); */
 }
 
 h4, p {
   text-align: center;
   margin: 0 auto;
-  padding: 2rem;
   font-weight: lighter;
   text-align: center;
+  color: white;
 }
 
 h4 {
-  font-weight: normal;
+  font-weight:lighter;
   font-size: 2rem;
 }
 
