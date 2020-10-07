bca3b060a37d11a0aeb3162c2e683d9a9c7e3818
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-10-07 21:19:43 +0100

    portfolio links stlying

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -13,17 +13,37 @@
       <div class="project-block-container solar">
         <h4 class="project-heading">Solar System App</h4>
           <p>
-            An interactive online browser application for the BBC targeted towards children. This app displays information on the planets in our solar system in a fun and interactive manner, with the information obtained from an external API.
-            It also hosts a quiz with a leaderboard and scores. Utilised Vue.js, Express, MongoDB, HTML and CSS.
+            An interactive online browser application for the BBC. This app displays information on the planets in our solar system in a fun and interactive manner,
+            with the information obtained from an external API. It also hosts a quiz with a leaderboard and scores.
           </p>
+          <ul>
+            <li>Utilised Vue.js, Express, MongoDB, HTML and CSS.</li>
+            <li>Libraries used include Bootstrap and Howler</li>
+          </ul>
+          <div class="link-project">
+            <a class="github-logo" href="https://github.com/JaneHope607/group_project_solar_system_app">
+              <img src="../assets/github-logo.png"/>
+            </a>
+          </div>
       </div>
 
       <div class="project-block-container gym">
         <h4>Total Fitness Gym App</h4>
           <p>
             A web application for a gym manager to manage memberships, classes and sessions. Built using Ruby and Sinatra, and writing data to an SQL database.
-            Full CRUD functionality. Utilised Ruby, Sinatra, SQL, HTML and CSS.
           </p>
+          <ul>
+            <li>Full CRUD functionality.</li>
+            <li>Utilised Ruby, Sinatra, SQL, HTML and CSS.</li>
+          </ul>
+          <div class="link-project">
+            <a class="github-logo" href="https://github.com/JaneHope607/codeclan_gym_app_project">
+              <img src="../assets/github-logo.png"/>
+            </a>
+            <a class="link-logo" href="https://total-fitness-gym-app.herokuapp.com/">
+              <img src="../assets/link.png"/>
+            </a>
+          </div>
       </div>
 
       <div class="project-block-container">    
@@ -38,9 +58,20 @@
         <h4 class="project-heading">Tyrannogotchi</h4>
           <p>
             A Tamagotchi-inspired web application where a user can create an account and take care of a virtual T-Rex by feeding and healing them.
-            Utilised JavaScript, React, Java, Spring, CSS and Bootstrap. User authentication with Okta
           </p>
-      </div>
+          <ul>
+            <li>Utilised JavaScript, React, Java, Spring, CSS and Bootstrap.</li>
+            <li>User authentication with Okta.</li>
+          </ul>
+          <div class="link-project">
+            <a class="github-logo" href="https://github.com/JaneHope607/tyrannogotch_project_frontend">
+              <img src="../assets/github-logo.png"/>
+            </a>
+            <a class="github-logo" href="https://github.com/JaneHope607/tyrannogotchi_project_backend">
+              <img src="../assets/github-logo.png"/>
+            </a>
+          </div> 
+        </div>
     </div>  
   </section>
 </template>
@@ -83,28 +114,55 @@ export default {
 }
 
 .project-block-container {
-  /* margin: 1rem; */
-  background-color: rgb(4, 48, 88);;
+  height: 100%;
+  display: block;
+  position: relative;
+}
+
+.project-block-container.gym {
+  background-color: rgb(4, 48, 88);
+  color: white;
 }
 
 .media-object {
   width: 100%;
-  height: auto;
+  height: 100%;
   text-align: center;
-  /* box-shadow: 5px 5px 15px hsla(210, 6%, 42%, 0.73); */
+  box-shadow: 5px 5px 15px hsla(210, 6%, 42%, 0.73);
 }
 
-h4, p {
+h4, p, ul {
   text-align: center;
   margin: 0 auto;
   font-weight: lighter;
   text-align: center;
-  color: white;
+}
+
+p {
+  padding: 0 1rem;
+  text-align: justify;
+  margin-bottom: 1rem;
+}
+
+li {
+  text-align: justify;
 }
 
 h4 {
   font-weight:lighter;
   font-size: 2rem;
+  padding: 1.5rem;
+}
+
+.github-logo {
+  display: inline-block;
+}
+
+.link-project {
+  position: absolute; 
+  bottom: 0.5rem;
+  display: flex;
+  margin: 0 auto;
 }
 
 </style>
